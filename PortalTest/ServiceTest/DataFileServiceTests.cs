206faf7066996f381deb70ae6db937b22c59e7d7
minhdatcvp <minhdatptt@gmail.com>
2025-03-28 00:16:38 +0700

    update UT

--- PortalTest/ServiceTest/DataFileServiceTests.cs
+++ PortalTest/ServiceTest/DataFileServiceTests.cs
@@ -3,6 +3,7 @@ using Moq;
 using PortalCommon.Model;
 using PortalRepository.Repository;
 using PortalService.Services;
+using System.Reflection;
 
 namespace PortalTest.ServiceTest
 {
@@ -20,11 +21,11 @@ namespace PortalTest.ServiceTest
         }
 
         [Fact]
-        public void GetData_ShouldReturnEmptyResponse_WhenNoDataAvailable()
+        public void GetChartData_ReturnsEmptyData_WhenNoDataAvailable()
         {
             _mockRepository.Setup(repo => repo.GetData()).Returns(new List<DataPoint>());
 
-            var result = _service.GetData();
+            var result = _service.GetChartData(null, null);
 
             Assert.NotNull(result);
             Assert.Empty(result.Data);
@@ -32,40 +33,70 @@ namespace PortalTest.ServiceTest
         }
 
         [Fact]
-        public void GetData_ShouldReturnValidSummary_WhenDataAvailable()
+        public void GetChartData_CalculatesCorrectSummary()
         {
-            var testData = new List<DataPoint>
+            var data = new List<DataPoint>
         {
             new DataPoint { Date = DateTime.Parse("2025-03-25 10:00"), Price = 100 },
             new DataPoint { Date = DateTime.Parse("2025-03-25 11:00"), Price = 200 },
             new DataPoint { Date = DateTime.Parse("2025-03-25 12:00"), Price = 150 }
         };
-            _mockRepository.Setup(repo => repo.GetData()).Returns(testData);
 
-            var result = _service.GetData();
+            _mockRepository.Setup(repo => repo.GetData()).Returns(data);
+
+            var result = _service.GetChartData(null, null);
 
             Assert.Equal(100, result.DataSummary.Min);
             Assert.Equal(200, result.DataSummary.Max);
             Assert.Equal(150, result.DataSummary.Average);
-            Assert.NotEqual("N/A", result.DataSummary.MostExpensiveHour);
+            Assert.NotNull(result.DataSummary.BestBuyTime);
+            Assert.NotNull(result.DataSummary.BestSellTime);
         }
 
         [Fact]
-        public void PagingData_ShouldReturnPagedData()
+        public void PagingData_ReturnsPagedResults()
         {
-            var testData = new List<DataPoint>
+            var data = Enumerable.Range(1, 10).Select(i => new DataPoint
+            {
+                Date = DateTime.Parse("2025-03-25").AddHours(i),
+                Price = i * 10
+            }).ToList();
+
+            _mockRepository.Setup(repo => repo.GetData()).Returns(data);
+
+            var (total, pagedData) = _service.PagingData(2, 3, "Price", "asc", null);
+
+            Assert.Equal(10, total);
+            Assert.Equal(3, pagedData.Count);
+            Assert.Equal(40, pagedData.First().Price);
+        }
+
+        [Fact]
+        public void PagingData_ThrowsException_WhenInvalidSortBy()
+        {
+            var data = new List<DataPoint> { new DataPoint { Date = DateTime.Now, Price = 100 } };
+            _mockRepository.Setup(repo => repo.GetData()).Returns(data);
+
+            Assert.Throws<ArgumentException>(() => _service.PagingData(1, 10, "InvalidProperty", "asc", null));
+        }
+
+        [Fact]
+        public void GetBestBuySellTimes_ReturnsCorrectTimes()
+        {
+            var data = new List<DataPoint>
         {
             new DataPoint { Date = DateTime.Parse("2025-03-25 10:00"), Price = 100 },
-            new DataPoint { Date = DateTime.Parse("2025-03-25 11:00"), Price = 200 },
-            new DataPoint { Date = DateTime.Parse("2025-03-25 12:00"), Price = 150 }
+            new DataPoint { Date = DateTime.Parse("2025-03-25 11:00"), Price = 50 },
+            new DataPoint { Date = DateTime.Parse("2025-03-25 12:00"), Price = 200 }
         };
-            _mockRepository.Setup(repo => repo.GetData()).Returns(testData);
 
-            var (total, pagedData) = _service.PagingData(1, 2, "Price", "asc", null);
+            var result = _service.GetType().GetMethod("GetBestBuySellTimes", BindingFlags.NonPublic | BindingFlags.Instance)
+                .Invoke(_service, new object[] { data });
 
-            Assert.Equal(3, total);
-            Assert.Equal(2, pagedData.Count);
-            Assert.Equal(100, pagedData.First().Price);
+            var (bestBuy, bestSell) = ((string, string))result;
+
+            Assert.Equal("2025-03-25 11:00", bestBuy);
+            Assert.Equal("2025-03-25 12:00", bestSell);
         }
     }
 }
